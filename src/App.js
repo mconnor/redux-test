cdf418f70d45689dcea06a54753a0108640ff51e
mike connor <mike@rocketnumber9.org>
2020-02-05 17:35:45 -0600

    middleware redux-logger

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 
 import  React from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import logo from './logo.svg';
+
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
+import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
 import rootReducer from './rootReducer'
 import Toggle from './Toggle'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import './App.css';
-
 import MoviesList from './MoviesList';
 import MovieDetail from './MovieDetail';
+import logo from './logo.svg';
+import './App.css';
+
+const middleware = [logger]
 
 const store = createStore(
     rootReducer,
     {},
-    composeWithDevTools(),
+    composeWithDevTools(applyMiddleware(...middleware)),
 );
 
 
