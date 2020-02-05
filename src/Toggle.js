ab91dc4dbb58a8b4e892374efc18f8266e44a524
mike connor <mike@rocketnumber9.org>
2020-02-05 17:03:00 -0600

    bind action creators

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,19 +1,24 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from 'react';
+import { connect } from 'react-redux';
 import { toggleMessage } from './actions';
+import { bindActionCreators } from 'redux';
 
-
-const Toggle = ({ messageVisibility, dispatch }) => (
-        <div>
-            {messageVisibility &&
-                <p>redux action toggled</p>
-            }
-            <button onClick={() => dispatch(toggleMessage())}>Toggle Me</button>
-        </div>
+const Toggle = ({ messageVisibility, toggleMessage }) => (
+	<div>
+		{messageVisibility && <p>redux action toggled</p>}
+		<button onClick={toggleMessage}>Toggle Me</button>
+	</div>
 );
 
 const mapStateToProps = (state) => ({
-    messageVisibility: state.message.messageVisibility,
-})
+	messageVisibility: state.message.messageVisibility
+});
 
-export default connect(mapStateToProps)(Toggle)
+const mapDispatchToProps = (dispatch) => bindActionCreators(
+		{
+			toggleMessage
+		},
+		dispatch
+	);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Toggle);
