988fd5184bf1da65038c17772aacc805fe1adcb8
mike connor <mike@rocketnumber9.org>
2020-02-06 11:44:31 -0600

    logo

--- src/App.js
+++ src/App.js
@@ -1,46 +1,40 @@
-
-import  React from "react";
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
-import { Provider } from 'react-redux'
-import { applyMiddleware, createStore } from 'redux'
-import logger from 'redux-logger'
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-import rootReducer from './rootReducer'
-import Toggle from './toggle/Toggle'
+import rootReducer from './rootReducer';
+import Toggle from './toggle/Toggle';
 import MoviesList from './movies/MoviesList';
 import MovieDetail from './movies/MovieDetail';
 import logo from './logo.svg';
 import './App.css';
 
-const middleware = [logger, thunk]
+const middleware = [ logger, thunk ];
 
-const store = createStore(
-    rootReducer,
-    {},
-    composeWithDevTools(applyMiddleware(...middleware)),
-);
-
+const store = createStore(rootReducer, {}, composeWithDevTools(applyMiddleware(...middleware)));
 
 const App = () => (
-    <Provider store={store}>
-        <Router>
-            <div className="App">
-                <header className="App-header">
-                    <Link to="/">
-                        <img src={logo} className="App-logo" alt="logo" />
-                    </Link>
-                </header>
-                <Toggle />
-                <Switch>
-                    <Route exact path="/" component={MoviesList} />
-                    <Route path="/:id" component={MovieDetail} />
-                </Switch>
-            </div>
-        </Router>
-    </Provider>
+	<Provider store={store}>
+		<Router>
+			<div className="App">
+				<header className="App-header">
+					<Link to="/">
+                        <h1 className="App-logo"><span>&#127775;</span>Mike's Movie Data Base <span>&#127775;</span></h1>
+					</Link>
+				</header>
+				<Toggle />
+				<Switch>
+					<Route exact path="/" component={MoviesList} />
+					<Route path="/:id" component={MovieDetail} />
+				</Switch>
+			</div>
+		</Router>
+	</Provider>
 );
 
 export default App;
